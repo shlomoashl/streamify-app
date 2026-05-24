1668d3a641657e4579b842cee15ca64a0cf8dab9
RUVENSHA <sa0548407548@gmail.com>
2026-05-24 20:52:15 +0300

    update

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { Capacitor } from '@capacitor/core';
+import { CapacitorUpdater } from '@capgo/capacitor-updater';
 import './index.css';
 
 const rootElement = document.getElementById('root');
@@ -9,9 +9,20 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// קריטי: לפני טעינת App
+if (Capacitor.isNativePlatform()) {
+  CapacitorUpdater.notifyAppReady()
+    .then(() => console.log("notifyAppReady OK - bootstrap"))
+    .catch(err => console.error("notifyAppReady failed - bootstrap", err));
+}
+
+// טוענים את App רק אחרי notify
+import('./App').then(({ default: App }) => {
+  const root = ReactDOM.createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
